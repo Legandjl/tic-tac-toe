499ee0b6005e14e4c98966ea693de0ba7dfd8018
dorian <dorianlegan@gmail.com>
2021-07-20 14:22:10 +0100

    functional but barebones - general logic flow works ok

--- script.js
+++ script.js
@@ -1,20 +1,43 @@
-let gameController = (() => {
+let displayController = (() => {
+
+    let cells = document.querySelectorAll(".gameCell");
+    let header = document.querySelector("#header");
 
     //switch between players each round
+
+    let resetDisplay = function () {
+
+        cells.forEach((cell) => {
+
+            cell.innerText = "";
+        })
+    }
 
     let boardClick = function (e) {
 
         let cell = e.target;
+        console.log(e.target.dataset.row)
 
         if (cell.innerText == "") {
 
             gameBoard.cellClick(cell);
         }
+    }
+
+    let setHeader = function(text) {
+
+        header.innerText = text;
 
     }
 
     let board = document.querySelector("#gameBoard");
     board.addEventListener("click", boardClick);
+
+    return {
+
+        resetDisplay,
+        setHeader
+    }
 
 })();
 
@@ -22,57 +45,109 @@ let gameController = (() => {
 let gameBoard = function () {
 
     let currentPlayer = Player("X");
-    let board = [];
-    let cells = document.querySelectorAll(".gameCell");
+
+    let board = [
+        ["", "", ""],
+        ["", "", ""],
+        ["", "", ""]
+    ];
+
+    let resetBoard = function () {
+
+        board = [
+            ["", "", ""],
+            ["", "", ""],
+            ["", "", ""]
+        ];
+
+        displayController.resetDisplay();
+
+    }
 
     let cellClick = function (cell) {
 
         cell.innerText = currentPlayer.getType();
+       
         //possibly add at a specific index?
-        board.push(currentPlayer.getType());
-        checkWinner();
+        board[cell.dataset.row][cell.dataset.id] = currentPlayer.getType();       
+        if(checkWinner()) {
+
+            resetBoard();
+        }
 
         if (currentPlayer.getType() == "X") {
 
             currentPlayer = Player("O");
+            displayController.setHeader("Player " + currentPlayer.getType() + " turn")
             return;
         }
 
         currentPlayer = Player("X");
+        displayController.setHeader("Player " + currentPlayer.getType() + " turn")
     }
 
     let checkWinner = function () {
 
-        if (board.length > 2) {
+        let winner = false;
 
-            //check for winner
-        }
-    }
+        board.forEach((element, index) => {
 
-    /*
+            console.log(index)
+
+            if ((element[0] == element[1] && element[0] == element[2]) && (element[0] != "")) {
+
+                console.log("winner " + element[0])
+                winner = true;
+                return;
+            }
+
+        })
+
+        board[0].forEach((element, index) => {
+
+            if ((element == board[1][index] && element == board[2][index]) && (element != "")) {
+
+                console.log("winner" + element)
+                winner = true;
+                return;
+            }
+
+            switch (index == 0 || index == 2) {
+
+                case index == 0:
+
+                    if (element == board[1][index + 1] && element == board[2][index + 2] && (element != "")) {
+
+                        console.log("winner!!" + element)
+                        winner = true;
+                        break;
+                    }
+
+                    case index == 2:
+
+                        if (element == board[1][index - 1] && element == board[2][index - 2] && (element != "")) {
+
+                            console.log("winner!" + element)
+                            winner = true;
+                            break;
+                        }
+            }
+
+
+        })
+
+        return winner;
 
     
-
-    let populate = function() {
-
-        cells.forEach((cell, index) => {
-            console.log(cell) 
-            cell.innerText = board[index];      
-        
-        });
-        
     }
-
-    */
 
     return {
-        cellClick
+
+        cellClick,
+        resetBoard
     }
 
-
 }();
-
-
 
 function Player(type) {
 
@@ -82,6 +157,7 @@ function Player(type) {
     }
 
     return {
+
         getType
     }
 
